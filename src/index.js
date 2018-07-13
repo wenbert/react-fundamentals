2a3f1ea78e5a4a553817a07111223c3e867b798a
wenbert <wenbert@gmail.com>
2018-07-13 21:19:47 +1200

    Some randomness for the game

--- src/index.js
+++ src/index.js
@@ -3,26 +3,75 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import registerServiceWorker from './registerServiceWorker';
+import {shuffle, sample} from 'underscore';
 
 const authors = [
     {
-        name: 'Mark Twain',
-        imageUrl: 'images/authors/marktwain.jpg',
-        imageSource: 'Wikimedia Commons',
-        books: [
-            'The Adventures of Huckleberry Finn',
-            'Life on the Mississippi',
-            'Roughing It',
-        ]
+      name: 'Mark Twain',
+      imageUrl: 'images/authors/marktwain.jpg',
+      imageSource: 'Wikimedia Commons',
+      books: ['The Adventures of Huckleberry Finn']
+    },
+    {
+      name: 'Joseph Conrad',
+      imageUrl: 'images/authors/josephconrad.png',
+      imageSource: 'Wikimedia Commons',
+      books: ['Heart of Darkness']
+    },
+    {
+      name: 'J.K. Rowling',
+      imageUrl: 'images/authors/jkrowling.jpg',
+      imageSource: 'Wikimedia Commons',
+      imageAttribution: 'Daniel Ogren',
+      books: ['Harry Potter and the Sorcerers Stone']
+    },
+    {
+      name: 'Stephen King',
+      imageUrl: 'images/authors/stephenking.jpg',
+      imageSource: 'Wikimedia Commons',
+      imageAttribution: 'Pinguino',
+      books: ['The Shining', 'IT']
+    },
+    {
+      name: 'Charles Dickens',
+      imageUrl: 'images/authors/charlesdickens.jpg',
+      imageSource: 'Wikimedia Commons',
+      books: ['David Copperfield', 'A Tale of Two Cities']
+    },
+    {
+      name: 'William Shakespeare',
+      imageUrl: 'images/authors/williamshakespeare.jpg',
+      imageSource: 'Wikimedia Commons',
+      books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
     }
-];
+  ];
 
-const state = {
-    turnData: {
-        author: authors[0],
-        books: authors[0].books
+function getTurnData(authors) {
+    const allBooks = authors.reduce(function (p, c, i) {
+        return p.concat(c.books);
+    }, []);
+    const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+    const answer = sample(fourRandomBooks);
+    return {
+        books: fourRandomBooks,
+        author: authors.find((author) => 
+            author.books.some((title) => 
+            title === answer
+            )
+        )
     }
 }
+
+const state = {
+    turnData: getTurnData(authors)
+}
+
+// const state = {
+//     turnData: {
+//         author: authors[0],
+//         books: authors[0].books
+//     }
+// }
 
 function render() {
     ReactDOM.render(<AuthorQuiz {...state} />,
